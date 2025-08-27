e9092c9bc2c2612d5d3793ddc5c7890826b515dd
Joshua Blew <joshua@normalfinance.io>
2025-08-27 12:01:08 -0500

    fix: remove IndexToken from auxiliary files

--- generate-bindings.sh
+++ generate-bindings.sh
@@ -2,8 +2,6 @@
 mkdir -p bindings
 
 # Generate bindings for each contract
-soroban contract bindings typescript --wasm target/wasm32v1-none/release/index_token.wasm --output-dir bindings/index_token --overwrite
-
 soroban contract bindings typescript --wasm target/wasm32v1-none/release/index.wasm --output-dir bindings/index --overwrite
 
 soroban contract bindings typescript --wasm target/wasm32v1-none/release/index_factory.wasm --output-dir bindings/index_factory --overwrite
